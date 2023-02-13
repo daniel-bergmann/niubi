82b4f94e555830190d6bcf32c6a0e9994ef2c138
daniel-bergmann <61001914+daniel-bergmann@users.noreply.github.com>
2023-02-13 19:50:33 +0000

    this is a niubi blog

---
+++ pages/api/blog.ts
@@ -0,0 +1,30 @@
+import type { NextApiRequest, NextApiResponse } from "next"
+import clientPromise from "../../lib/mongodb"
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<any>
+) {
+  try {
+    const client = await clientPromise
+    const db = client.db("blog")
+    if (req.method === "POST") {
+      // Process a POST request
+      const post = req.body
+      await db.collection("posts").insertOne(post)
+      await res.status(200).json({ message: "Post added" })
+    } else if (req.method === "GET") {
+      // Process a GET request
+      const blogPosts = await db
+        .collection("posts")
+        .find({})
+        .sort({ metacritic: -1 })
+        .limit(10)
+        .toArray()
+      // Process a GET request
+      res.status(200).json(blogPosts)
+    }
+  } catch (e) {
+    console.error(e)
+  }
+}
